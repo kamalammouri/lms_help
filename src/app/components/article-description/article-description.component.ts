a007d0d3f59bac2eefab715b71c5b4064188087b
Kamal <kamalammouri1994@gmail.com>
2022-06-09 17:45:32 +0100

    add inner html in descreption

--- src/app/components/article-description/article-description.component.ts
+++ src/app/components/article-description/article-description.component.ts
@@ -8,6 +8,19 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ArticleDescriptionComponent implements OnInit {
   @Input('articleDesc') articleDesc:any;
   @Input('articleLabel') articleLabel:any;
+  test = `<p class="h2" style="color:red">
+  This paragraph has multiple lines.
+  <em>But</em> HTML reduces them to a single line,
+  omitting the carriage return we have used.
+</p>
+
+
+
+<p>
+  This paragraph has multiple spaces.
+  But HTML reduces them all to a single
+  space, omitting the extra spaces and line we have used.
+</p>`
   constructor() { }
 
   ngOnInit(): void {
